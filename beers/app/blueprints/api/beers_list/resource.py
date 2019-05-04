8633ec079946af9c9d7a3720edf52aad650968ac
fcschmidt <fcschimidt@gmail.com>
2019-05-04 12:40:39 -0300

    Reformad type

--- beers/app/blueprints/api/beers_list/resource.py
+++ beers/app/blueprints/api/beers_list/resource.py
@@ -24,8 +24,8 @@ beers_parser.add_argument('beer_name', type=str)
 beers_parser.add_argument('description', type=str)
 beers_parser.add_argument('harmonization', type=str)
 beers_parser.add_argument('color', type=str)
-beers_parser.add_argument('alcohol', type=str)
-beers_parser.add_argument('temperature', type=str)
+beers_parser.add_argument('alcohol', type=float)
+beers_parser.add_argument('temperature', type=float)
 
 resource_fields_beer = {
     'id': fields.Integer,
@@ -33,8 +33,8 @@ resource_fields_beer = {
     'description': fields.String,
     'harmonization': fields.String,
     'color': fields.String,
-    'alcohol': fields.String,
-    'temperature': fields.String
+    'alcohol': fields.Float,
+    'temperature': fields.Float
 }
 
 
