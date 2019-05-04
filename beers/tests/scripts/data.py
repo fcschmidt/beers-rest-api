10efa2c942974cfc0b479694576e2f13986a2e32
fcschmidt <fcschimidt@gmail.com>
2019-05-04 10:47:14 -0300

    Renamed data for using populate

--- beers/tests/scripts/data.py
+++ beers/tests/scripts/data.py
@@ -5,50 +5,38 @@
 beers_data = [
     {
         'beer_name': 'Skoll',
-        'description': 'A Cerveja Adriática 600ml foi criada pelo alemão Henrique Thielen, um visionário '
-                       'cervejeiro do início do século XX, ela teve seu nome em homenagem a '
-                       'cervejaria que traduz toda uma era de tradição passada de pai para filho. '
-                       'Hoje, conhecida como a irmã mais velha da Original, '
-                       'ela é uma cerveja pedida certa para a mesa de bar. '
-                       'Reconhecidamente uma cerveja puro malte de alta qualidade, '
-                       'fácil de beber e com aromas especiais que dão um toque equilibrado!.',
-        'harmonization': 'Ela é leve e refrescante, por isso harmoniza muito bem com pratos leves! '
-                         'Assim, um sabor não vai sobrepor o outro!',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
+        'harmonization': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
         'color': 'clara',
-        'alcohol': '3',
-        'temperature': '4'
+        'alcohol': 3,
+        'temperature': 4
     },
     {
         'beer_name': 'Malzibier',
-        'description': 'A Cerveja Adriática 600ml foi criada pelo alemão Henrique Thielen, '
-                       'um visionário cervejeiro do início do século XX, ela teve seu nome em homenagem a '
-                       'cervejaria que traduz toda uma era de tradição passada de pai para filho. '
-                       'Hoje, conhecida como a irmã mais velha da Original, ela é uma cerveja pedida '
-                       'certa para a mesa de bar. Reconhecidamente uma cerveja puro malte de alta qualidade,'
-                       ' fácil de beber e com aromas especiais que dão um toque equilibrado!.',
-        'harmonization': 'Ela é leve e refrescante, por isso harmoniza muito bem com pratos leves! '
-                         'Assim, um sabor não vai sobrepor o outro!',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
+        'harmonization': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
         'color': 'clara',
-        'alcohol': '2,5',
-        'temperature': '0'
+        'alcohol': 2.5,
+        'temperature': 0
     },
     {
         'beer_name': 'Brahma',
-        'description': 'A Cerveja Adriática 600ml foi criada pelo alemão Henrique Thielen, um visionário cervejeiro do início do século XX, ela teve seu nome em homenagem a cervejaria que traduz toda uma era de tradição passada de pai para filho. Hoje, conhecida como a irmã mais velha da Original, ela é uma cerveja pedida certa para a mesa de bar. Reconhecidamente uma cerveja puro malte de alta qualidade, fácil de beber e com aromas especiais que dão um toque equilibrado!.',
-        'harmonization': 'Ela é leve e refrescante, por isso harmoniza muito bem com pratos leves! Assim, um sabor não vai sobrepor o outro!',
+        'description':  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
+        'harmonization': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
         'color': 'escura',
-        'alcohol': '2,5',
-        'temperature': '0'
+        'alcohol': 2.5,
+        'temperature': 0
     }
 ]
+
 update_beer = {
     'beer_name': 'Kaiser',
-    'description': 'A Cerveja kaiser 600ml foi criada pelo alemão Henrique Thielen, um visionário cervejeiro do início do século XX, ela teve seu nome em homenagem a cervejaria que traduz toda uma era de tradição passada de pai para filho. Hoje, conhecida como a irmã mais velha da Original, ela é uma cerveja pedida certa para a mesa de bar. Reconhecidamente uma cerveja puro malte de alta qualidade, fácil de beber e com aromas especiais que dão um toque equilibrado!.',        'harmonization': 'Ela é leve e refrescante, por isso harmoniza muito bem com pratos leves! Assim, um sabor não vai sobrepor o outro!',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
+    'harmonization': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
     'color': 'clara',
-    'alcohol': '3',
-    'temperature': '1'
+    'alcohol': 3,
+    'temperature': 1
 }
-
 
 ingredients_data = [
     {
