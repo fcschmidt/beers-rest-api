ed6276dd5363ec9cbadb8e971635f6999128cf16
fcschmidt <fcschimidt@gmail.com>
2019-06-04 21:50:13 -0300

    Updated Tests

--- beers/tests/test_beers_list_api_resources.py
+++ beers/tests/test_beers_list_api_resources.py
@@ -1,136 +1,117 @@
+import pytest
 from beers.tests.utils.populate_data_base import populate_beers
 
+from beers.tests.dictionaries.expected_beers_responses import (
+    EXPECTED_DB_EMPTY,
+    EXPECTED_BEER_NOT_EXIST,
+    EXPECTED_KAISER_NOT_EXIST,
+    EXPECTED_COLOR_NOT_EXIST,
+    EXPECTED_ALCOHOL_NOT_EXIST,
+    EXPECTED_TEMPERATURE_NOT_EXIST,
+    EXPECTED_INGREDIENT_NOT_EXIST
+    )
 
-def test_get_not_beers(client, session):
-    response = client.get('/api/v1/beers')
-    assert response.status_code == 200
-
-    expected = {
-        'message': 'Data base empty!'
-    }
-    resp_json = response.get_json()
-    assert resp_json['message'] == expected['message']
-
-
-def test_get_beers(client, session):
-    populate_beers(3)
-    response = client.get('/api/v1/beers')
-    assert response.status_code == 200
-    resp_json = response.get_json()
-    assert len(resp_json) == 3
+API_URL = '/api/v1/beers'
 
 
-def test_filter_beer_by_name_not_exist(client, session):
-    populate_beers(3)
-    response = client.get('/api/v1/beers?beer_name=kaiser')
-    assert response.status_code == 404
+@pytest.mark.usefixtures('session')
+class TestBeersAPIListFilterResponseSuccessfully:
+    """Test Beers API Response List and Filter Items"""
 
-    expected = {
-        'error': 'kaiser does not exist.'
-    }
-    resp_json = response.get_json()
-    assert resp_json['error'] == expected['error']
+    def test_get_not_beers(self, client):
+        response = client.get(f'{API_URL}')
+        assert response.status_code == 200
+        response_json = response.get_json()
+        assert response_json == EXPECTED_DB_EMPTY
 
+    def test_get_beers(self, client):
+        populate_beers(3)
+        response = client.get(f'{API_URL}')
+        assert response.status_code == 200
+        response_json = response.get_json()
+        assert len(response_json) == 3
 
-def test_filter_beer_by_name(client, session):
-    populate_beers(3)
-    response = client.get('/api/v1/beers?beer_name=Skoll')
-    assert response.status_code == 200
+    def test_filter_beer_by_name(self, client):
+        populate_beers(3)
+        response = client.get(f'{API_URL}?beer_name=Skoll')
+        assert response.status_code == 200
 
+    def test_filter_beer_by_color(self, client):
+        populate_beers(3)
+        response = client.get(f'{API_URL}?color=clara')
+        assert response.status_code == 200
+        resp_json = response.get_json()
+        assert len(resp_json) == 2
 
-def test_filter_beer_by_color_not_exist(client, session):
-    populate_beers(3)
-    response = client.get('/api/v1/beers?color=azul')
-    assert response.status_code == 404
+    def test_filter_beer_by_alcohol(self, client):
+        populate_beers(3)
+        response = client.get(f'{API_URL}?alcohol=3')
+        assert response.status_code == 200
 
-    expected = {
-        'error': 'azul does not exist.'
-    }
-    resp_json = response.get_json()
-    assert resp_json == expected
+    def test_filter_beer_by_temperature(self, client):
+        populate_beers(3)
+        response = client.get(f'{API_URL}?temperature=4')
+        assert response.status_code == 200
 
+    def test_filter_beer_by_ingredient(self, client):
+        populate_beers(3)
+        response = client.get(f'{API_URL}?ingredient=cevada')
+        assert response.status_code == 200
 
-def test_filter_beer_by_color(client, session):
-    populate_beers(3)
-    response = client.get('/api/v1/beers?color=clara')
-    assert response.status_code == 200
-    resp_json = response.get_json()
-    assert len(resp_json) == 2
+        resp_json = response.get_json()
+        assert len(resp_json) == 3
 
+    def test_get_beer(self, client):
+        populate_beers(3)
+        response = client.get(f'{API_URL}/1')
+        assert response.status_code == 200
 
-def test_filter_beer_by_alcohol_not_exist(client, session):
-    populate_beers(3)
-    response = client.get('/api/v1/beers?alcohol=10')
-    assert response.status_code == 404
-
-    expected = {
-        'error': '10.0 does not exist.'
-    }
-    resp_json = response.get_json()
-    assert resp_json == expected
-
-
-def test_filter_beer_by_alcohol(client, session):
-    populate_beers(3)
-    response = client.get('/api/v1/beers?alcohol=3')
-    assert response.status_code == 200
-
-
-def test_filter_beer_by_temperature_not_exist(client, session):
-    populate_beers(3)
-    response = client.get('/api/v1/beers?temperature=10')
-    assert response.status_code == 404
-
-    expected = {
-        'error': '10.0 does not exist.'
-    }
-    resp_json = response.get_json()
-    assert resp_json == expected
+        resp_json = response.get_json()
+        assert resp_json['id'] == 1
 
 
-def test_filter_beer_by_temperature(client, session):
-    populate_beers(3)
-    response = client.get('/api/v1/beers?temperature=4')
-    assert response.status_code == 200
+@pytest.mark.usefixtures('session')
+class TestBeersAPIListFilterResponseErrors:
+    """Test Beers API Response Errors List and Filter Items"""
 
+    def test_get_beer_not_exist(self, client):
+        populate_beers(3)
+        response = client.get(f'{API_URL}/4')
+        assert response.status_code == 404
+        response_json = response.get_json()
+        assert response_json == EXPECTED_BEER_NOT_EXIST
 
-def test_filter_beer_by_ingredient_not_exist(client, session):
-    populate_beers(3)
-    response = client.get('/api/v1/beers?ingredient=uva')
-    assert response.status_code == 404
+    def test_filter_beer_by_name_not_exist(self, client):
+        populate_beers(3)
+        response = client.get(f'{API_URL}?beer_name=kaiser')
+        assert response.status_code == 404
+        response_json = response.get_json()
+        assert response_json == EXPECTED_KAISER_NOT_EXIST
 
-    expected = {
-        'error': 'uva does not exist.'
-    }
-    resp_json = response.get_json()
-    assert resp_json == expected
+    def test_filter_beer_by_color_not_exist(self, client):
+        populate_beers(3)
+        response = client.get('/api/v1/beers?color=blue')
+        assert response.status_code == 404
+        response_json = response.get_json()
+        assert response_json == EXPECTED_COLOR_NOT_EXIST
 
+    def test_filter_beer_by_alcohol_not_exist(self, client):
+        populate_beers(3)
+        response = client.get('/api/v1/beers?alcohol=10')
+        assert response.status_code == 404
+        response_json = response.get_json()
+        assert response_json == EXPECTED_ALCOHOL_NOT_EXIST
 
-def test_filter_beer_by_ingredient(client, session):
-    populate_beers(3)
-    response = client.get('/api/v1/beers?ingredient=cevada')
-    assert response.status_code == 200
+    def test_filter_beer_by_temperature_not_exist(self, client):
+        populate_beers(3)
+        response = client.get(f'{API_URL}?temperature=55')
+        assert response.status_code == 404
+        response_json = response.get_json()
+        assert response_json == EXPECTED_TEMPERATURE_NOT_EXIST
 
-    resp_json = response.get_json()
-    assert len(resp_json) == 3
-
-
-def test_get_beer_not_exist(client, session):
-    populate_beers(3)
-    response = client.get('/api/v1/beers/10')
-    assert response.status_code == 404
-
-    expected = {
-        'error': '10 does not exist.'
-    }
-    resp_json = response.get_json()
-    assert resp_json['error'] == expected['error']
-
-
-def test_get_beer(client, session):
-    populate_beers(3)
-    response = client.get('/api/v1/beers/1')
-    assert response.status_code == 200
-
-    resp_json = response.get_json()
-    assert resp_json['id'] == 1
+    def test_filter_beer_by_ingredient_not_exist(self, client):
+        populate_beers(3)
+        response = client.get(f'{API_URL}?ingredient=uva')
+        assert response.status_code == 404
+        response_json = response.get_json()
+        assert response_json == EXPECTED_INGREDIENT_NOT_EXIST
