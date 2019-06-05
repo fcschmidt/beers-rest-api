949393bcc2a2e0100591b214e337c6a58f588fec
fcschmidt <fcschimidt@gmail.com>
2019-06-04 21:49:20 -0300

    Renamed dict for data test beers

--- beers/tests/dictionaries/expected_beers_responses.py
+++ beers/tests/dictionaries/expected_beers_responses.py
@@ -21,3 +21,27 @@ EXPECTED_BEER_NOT_EXIST_INGREDIENT = {
 EXPECTED_BEER_DETETE_NOT_EXIST = {
     'error': '10 does not exist.'
 }
+
+EXPECTED_DB_EMPTY = {
+    'message': 'Data base empty!'
+}
+
+EXPECTED_KAISER_NOT_EXIST = {
+    'error': 'kaiser does not exist.'
+}
+
+EXPECTED_COLOR_NOT_EXIST = {
+    'error': 'blue does not exist.'
+}
+
+EXPECTED_ALCOHOL_NOT_EXIST = {
+    'error': '10.0 does not exist.'
+}
+
+EXPECTED_TEMPERATURE_NOT_EXIST = {
+    'error': '55.0 does not exist.'
+}
+
+EXPECTED_INGREDIENT_NOT_EXIST = {
+    'error': 'uva does not exist.'
+}
