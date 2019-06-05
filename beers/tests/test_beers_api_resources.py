92363b675adbbe72670d10c052a442bf943684d6
fcschmidt <fcschimidt@gmail.com>
2019-06-04 21:20:17 -0300

    Addded new format tests

--- beers/tests/test_beers_api_resources.py
+++ beers/tests/test_beers_api_resources.py
@@ -1,135 +1,71 @@
+import pytest
 from beers.tests.utils.populate_data_base import populate_beers
 
-api_url = '/api/v1/beers'
+from beers.tests.dictionaries.beers_data_test import (
+    NEW_BEER_DATA,
+    UPDATE_BEER_DATA,
+    UPDATE_BEER_NOT_EXIST,
+    UPDATE_BEER_NOT_EXIST_INGREDIENT,
+    )
+
+from beers.tests.dictionaries.expected_responses import (
+    EXPECTED_BEER_CREATED_SUCCESSFULLY,
+    EXPECTED_BEER_UPDATE_SUCCESSFULLY,
+    EXPECTED_BEER_DELETED_SUCCESSFULLY,
+    EXPECTED_BEER_NOT_EXIST,
+    EXPECTED_BEER_NOT_EXIST_INGREDIENT,
+    EXPECTED_BEER_DETETE_NOT_EXIST
+    )
+
+API_URL = '/api/v1/beers'
 
 
-def test_create_new_beer(client, session):
-    new_beer = {
-        'beer_name': 'Skoll',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
-        'harmonization': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
-        'color': 'clara',
-        'alcohol': '3',
-        'temperature': '4',
-        'ingredients': [
-            {'names': ['cevada', 'lupulo', 'malte', 'trigo']}
-        ]
-    }
-    response = client.post(api_url, json=new_beer)
-    assert response.status_code == 201
+@pytest.mark.usefixtures('session')
+class TestBeersAPIResponseSuccessfully:
+    """Test Beers API Response Successfully."""
 
-    resp_json = response.get_json()
-    expected = {
-        'message': 'Beer created successfully!'
-    }
-    assert resp_json == expected
+    def test_create_new_beer(self, client):
+        response = client.post(API_URL, json=NEW_BEER_DATA)
+        assert response.status_code == 201
+        response_json = response.get_json()
+        assert response_json == EXPECTED_BEER_CREATED_SUCCESSFULLY
 
+    def test_update_beer(self, client):
+        populate_beers(3)
+        response = client.put(f'{API_URL}/1', json=UPDATE_BEER_DATA)
+        assert response.status_code == 200
+        response_json = response.get_json()
+        assert response_json == EXPECTED_BEER_UPDATE_SUCCESSFULLY
 
-def test_update_beer_not_exist(client, session):
-    populate_beers(2)
-    update_beer = {
-        'beer_name': 'Skoll',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
-        'harmonization': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
-        'color': 'clara',
-        'alcohol': '3',
-        'temperature': '4',
-        'ingredients': {
-            'list': [
-                {'ingredient_name': 'cevada', 'id': 1},
-                {'ingredient_name': 'lupulo', 'id': 2},
-                {'ingredient_name': 'malte', 'id': 3},
-            ]
-        }
-    }
-    _id = 4
-    response = client.put(f'{api_url}/{_id}', json=update_beer)
-    assert response.status_code == 404
-
-    expected = {
-        'error': '4 does not exist.'
-    }
-    resp_json = response.get_json()
-    assert resp_json == expected
+    def test_delete_beer(self, client):
+        populate_beers(3)
+        response = client.delete(f'{API_URL}/1')
+        assert response.status_code == 202
+        response_json = response.get_json()
+        assert response_json == EXPECTED_BEER_DELETED_SUCCESSFULLY
 
 
-def test_update_beer_not_exist_ingredients(client, session):
-    populate_beers(2)
-    update_beer = {
-        'beer_name': 'Skoll',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
-        'harmonization': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
-        'color': 'clara',
-        'alcohol': '3',
-        'temperature': '4',
-        'ingredients': {
-            'list': [
-                {'ingredient_name': 'cevada', 'id': 55},
-                {'ingredient_name': 'lupulo', 'id': 100},
-                {'ingredient_name': 'malte', 'id': 49},
-            ]
-        }
-    }
-    _id = 1
-    response = client.put(f'{api_url}/{_id}', json=update_beer)
-    assert response.status_code == 404
+@pytest.mark.usefixtures('session')
+class TestBeersAPIResponseErrors:
+    """Test Beers API Response Errors."""
 
-    expected = {
-        'error': '55 does not exist.'
-    }
-    resp_json = response.get_json()
-    assert resp_json == expected
+    def test_update_beer_not_exist(self, client):
+        populate_beers(2)
+        response = client.put(f'{API_URL}/4', json=UPDATE_BEER_NOT_EXIST)
+        assert response.status_code == 404
+        response_json = response.get_json()
+        assert response_json == EXPECTED_BEER_NOT_EXIST
 
+    def test_update_beer_not_exist_ingredients(self, client):
+        populate_beers(2)
+        response = client.put(f'{API_URL}/1', json=UPDATE_BEER_NOT_EXIST_INGREDIENT)
+        assert response.status_code == 404
+        response_json = response.get_json()
+        assert response_json == EXPECTED_BEER_NOT_EXIST_INGREDIENT
 
-def test_update_beer(client, session):
-    populate_beers(3)
-    update_beer = {
-        'beer_name': 'Adriática',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
-        'harmonization': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius quis ante nec.',
-        'color': 'clara',
-        'alcohol': '5',
-        'temperature': '5',
-        'ingredients': {
-            'list': [
-                {'ingredient_name': 'cevada', 'id': 1},
-                {'ingredient_name': 'lupulo', 'id': 2},
-                {'ingredient_name': 'malte', 'id': 3},
-            ]
-        }
-    }
-    _id = 1
-    response = client.put(f'{api_url}/{_id}', json=update_beer)
-    assert response.status_code == 200
-
-    expected = {
-        'message': 'Beer updated successfully!'
-    }
-    resp_json = response.get_json()
-    assert resp_json == expected
-
-
-def test_delete_beer_not_exist(client, session):
-    populate_beers(3)
-    _id = 10
-    response = client.delete(f'{api_url}/{_id}')
-    assert response.status_code == 404
-
-    expected = {
-        'error': '10 does not exist.'
-    }
-    resp_json = response.get_json()
-    assert resp_json == expected
-
-
-def test_delete_beer(client, session):
-    populate_beers(3)
-    _id = 1
-    response = client.delete(f'{api_url}/{_id}')
-    assert response.status_code == 202
-
-    expected = {
-        'message': 'Beer deleted successfully!'
-    }
-    resp_json = response.get_json()
-    assert resp_json == expected
+    def test_delete_beer_not_exist(self, client):
+        populate_beers(3)
+        response = client.delete(f'{API_URL}/10')
+        assert response.status_code == 404
+        response_json = response.get_json()
+        assert response_json == EXPECTED_BEER_DETETE_NOT_EXIST
