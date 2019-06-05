b32920c7897e8dd58ff45140517512985443ef8a
fcschmidt <fcschimidt@gmail.com>
2019-06-05 10:54:28 -0300

    Modified responses

--- beers/app/blueprints/api/responses.py
+++ beers/app/blueprints/api/responses.py
@@ -1,28 +1,37 @@
 from flask import jsonify
 
 
-def resp_successfully(msg, status):
-    """
-    Response 201: for Created successfully!
-    Response 200: for Updated successfully!
-    Response 202: for Deleted successfully!
-    """
-    message = {
-        201: {'message': f'{msg} created successfully!'},
-        200: {'message': f'{msg} updated successfully!'},
-        202: {'message': f'{msg} deleted successfully!'}
-    }
-    response = jsonify(
-        message[status]
-    )
-    response.status_code = status
-    return response
-
-
 def resp_content_successfully(content):
     """Response 200"""
     response = jsonify(content)
     response.status_code = 200
+    return response
+
+
+def resp_created_successfully(resource):
+    """Response 201"""
+    response = jsonify({
+        'message': f'{resource} created successfully!'
+    })
+    response.status_code = 201
+    return response
+
+
+def resp_updated_successfully(resource):
+    """Response 201"""
+    response = jsonify({
+        'message': f'{resource} updated successfully!'
+    })
+    response.status_code = 201
+    return response
+
+
+def resp_deleted_successfully(resource):
+    """Response 202"""
+    response = jsonify({
+        'message': f'{resource} deleted successfully!'
+    })
+    response.status_code = 202
     return response
 
 
